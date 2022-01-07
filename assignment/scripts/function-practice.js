277344b089c0af5e2a4dc5e450f8b5d463079161
Brandon Lanier <brandonjlanier@gmail.com>
2022-01-06 22:13:46 -0600

    Picked and solved a problem from edabit

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -19,13 +19,12 @@ function helloName(name) {
 }
 // Remember to call the function to test
 
-helloName('Brandon');
 
 console.log(`Testing with my name, ${helloName('Brandon')}`);
-console.log('Testing helloName function:', helloName('Jake'));
+console.log('Testing helloName function again:', helloName('Jake'));
 
 // 3. Function to add two numbers together & return the result
-function addNumbers( firstNumber, secondNumber ) {
+function addNumbers(firstNumber, secondNumber) {
   // return firstNumber + secondNumber;
   let totalNumber = firstNumber + secondNumber;
   return totalNumber;
@@ -36,7 +35,7 @@ console.log('Testing addNumbers: 10 + 15 =', addNumbers(10, 15));
 console.log('Testing addNumbers: 1 + 2 =', addNumbers(1, 2));
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree( num1, num2, num3 ){
+function multiplyThree(num1, num2, num3){
   let totalMultiply = num1 * num2 * num3;
   return totalMultiply;
 }
@@ -47,8 +46,8 @@ console.log('Testing multiplyThree: 10 * 10 * 10 =', multiplyThree(10, 10, 10));
 
 // 5. Function that will return true if a number is positive,
 //    or greater than zero, and false otherwise
-function isPositive( number ) {
-  if ( number > 0 ){
+function isPositive(number) {
+  if (number > 0){
     return true;
   }
   else {
@@ -70,36 +69,44 @@ console.log('Testing isPositive: -10 is argument and should be false', isPositiv
 // 6. Function to return the _last_ item in an array. If the
 //    array is empty, return `undefined`.
 
-function getLast( array ) {
-  return array[array.length - 1];
+function getLast(inputArray) {
+  return inputArray[inputArray.length - 1];
 }
 
-console.log('Testing getLast function: Last item should be 7:', getLast([1, 2, 3, 4, 7]));
-console.log('Testing getLast function: Last item should be Maybe:', getLast(['Yes', 'No', 'Maybe']));
-console.log('Testing getLast function: Should say undefined', getLast([]));
+let numberTest = [1, 2, 3, 4, 7];
+let stringTest = ['Yes', 'No', 'Maybe'];
+let undefinedTest = [];
+
+console.log('Testing getLast function: Last item should be 7:', getLast(numberTest));
+console.log('Testing getLast function: Last item should be Maybe:', getLast(stringTest));
+console.log('Testing getLast function: Should say undefined', getLast(undefinedTest));
 
 // 7. Function to find a value in an array. Return true if the
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find
 
-function find( value, array ) {
-  for ( arrayItem of array) {
-    if ( arrayItem === value)
+let numberFinder = [21, 2, 3, 4, 5, 66, 22, 1, 99];
+let animals = ['cat', 'bird', 'dog', 'fish', 'chicken'];
+
+function find(value, array) {
+  for (let arrayItem of array) {
+    if (arrayItem === value)
     return true;
     }
     return false;
   }
 
-console.log('Testing True: Value is 5 and array contains 5', find(5, [1, 2, 3, 4, 5]));
-console.log('Testing False: Value is 8 and array has no 8', find(8, [66, 3, 22, 1, 99]));
-console.log('Testing True with string: find a dog in array?', find('dog', ['cat', 'bird', 'dog', 'fish']));
-console.log('Testing False with string: find monkey in array?', find('monkey', ['cat', 'bird', 'dog', 'fish']));
+console.log('Testing True: Value is 5 and array contains 5', find(5, numberFinder));
+console.log('Testing False: Value is 8 and array has no 8', find(8, numberFinder));
+console.log('Testing True with string: find a dog in array?', find('dog', animals));
+console.log('Testing False with string: find monkey in array?', find('monkey', animals));
 
 // ----------------------
 // Stretch Goals
 // ----------------------
 // 8. Function to check if a letter is the first letter in a
 //    string. Return true if it is, and false otherwise
+
 function isFirstLetter(letter, string) {
   if (string.charAt(0) === letter) {
     return true;
@@ -114,18 +121,20 @@ console.log( 'isFirstLetter - should say false', isFirstLetter('f', 'brandon') )
 console.log( 'isFirstLetter - should say true', isFirstLetter('q', 'queso') );
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll( numbers ) {
-  let sum = 0
+
+function sumAll(numbers) {
+  let sum = 0;
   // TODO: loop to add items
   for (let i = 0; i < numbers.length; i++) {
   sum += numbers[i];
-}
+  }
   return sum;
 }
 
 let testDigits = [5, 5, 5, 10];
-console.log(`Testing by putting a pre-defined array in: ${sumAll(testDigits)}`);
-console.log(`Testing by adding array numbers directly into the function argument: ${sumAll([1, 2, 3, 4])}`);
+
+console.log(`Testing by putting a pre-defined array in (Should be 25): ${sumAll(testDigits)}`);
+console.log(`Testing by adding array numbers directly into the function argumen (Should be 10): ${sumAll([1, 2, 3, 4])}`);
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
@@ -133,9 +142,9 @@ console.log(`Testing by adding array numbers directly into the function argument
 
 let inputArray = [11, 4, -3, -10, -19, 6, -14, 51];
 
-function newArray(input) {
+function newArray(digits) {
   let posArray = [];
-  for (let number of input) {
+  for (let number of digits) {
     if (number > 0) {
       posArray.push(number);
     }
@@ -144,8 +153,23 @@ function newArray(input) {
 }
 
 console.log(`This is the input array: ${inputArray}`);
-console.log(`This is the new array after running the function ${newArray(inputArray)}`);
+console.log(`This is the new array after calling the function ${newArray(inputArray)}`);
 
 // 11. Pick a problem from Edabit(https://edabit.com/) or
 //     CodeWars(https://www.codewars.com/). Then describe it
 //     here in a comment, write the function, and test it!
+
+// I did one of the medium tests called "Tile Teamwork Tactics".
+// The object is to see if your dice (represented by 'a') can reach your partners number (represented by 'b').
+// You have one more roll to reach your partners number, so the object is to see if it is possible with a 6 sided dice.  Your partner does not get another roll.
+
+
+function possibleBonus(a, b) {
+	let diff = Math.abs(a - b); // I had to look up how to easily calculate the difference between two numbers for this.
+	if (diff <= 6 && b > a) { // The difference has to be less than or equal to 6 while the players number must be greater or you won't be able to land on their number.
+		return true;
+	}
+	else {
+		return false; // false if b is too many numbers away or you are already greater than your partner.
+	}
+}
